645df8fd5c09f0dfcaf8c83d2b3a0bbade96f6b7
ronyhe <ronyheruth@wix.com>
2026-01-19 23:26:24 +0200

    Add more tests

--- test.test.ts
+++ test.test.ts
@@ -1,8 +1,14 @@
 import { expect, test } from 'vitest'
 import { runElection, type Inputs } from './elect.js'
-import data from './knesset25.json' with { type: 'json' }
+import data24 from './knesset24.json' with { type: 'json' }
+import data25 from './knesset25.json' with { type: 'json' }
+
+test('24th Knesset', () => {
+    const results = runElection(data24 as unknown as Inputs)
+    expect(results.seats).toEqual(data24.expected)
+})
 
 test('25th Knesset', () => {
-    const results = runElection(data as unknown as Inputs)
-    expect(results.seats).toEqual(data.expected)
+    const results = runElection(data25 as unknown as Inputs)
+    expect(results.seats).toEqual(data25.expected)
 })
