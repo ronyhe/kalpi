c8aa825cd27fb52ea5afe5be053ec66b09cd7920
ronyhe <ronyheruth@wix.com>
2026-01-19 11:21:50 +0200

    setup ox

--- elect.ts
+++ elect.ts
@@ -1,3 +1,23 @@
+const ISRAEL_RULES = {
+    totalSeats: 120,
+    threshold: 0.0325, // 3.25%
+}
+
+interface Options {
+    votes: {
+        [party: string]: number;
+    },
+    threshold: number; // Achuz HaChasima
+    remainderPacts: string[][]; // Heskemei Odafim
+    totalSeats: number; // 120 seats in Knesset
+}
+
+interface Results {
+    seats: {
+        [party: string]: number;
+    }
+}
+
 async function main() {
     console.log("Election process started.");
 }
