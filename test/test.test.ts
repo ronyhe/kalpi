78a3b5105d2975897e675529cc7dd431975550a5
ronyhe <ronyheruth@wix.com>
2026-01-20 00:12:49 +0200

    use node test

--- test/test.test.ts
+++ test/test.test.ts
@@ -1,14 +1,15 @@
-import { expect, test } from 'vitest'
-import { runElection, type Inputs } from '../src/elect.js'
+import { test } from 'node:test'
+import assert from 'node:assert/strict'
+import { runElection, type Inputs } from '../src/elect.ts'
 import data24 from './knesset24.json' with { type: 'json' }
 import data25 from './knesset25.json' with { type: 'json' }
 
 test('24th Knesset', () => {
     const results = runElection(data24 as unknown as Inputs)
-    expect(results.seats).toEqual(data24.expected)
+    assert.deepEqual(results.seats, data24.expected)
 })
 
 test('25th Knesset', () => {
     const results = runElection(data25 as unknown as Inputs)
-    expect(results.seats).toEqual(data25.expected)
+    assert.deepEqual(results.seats, data25.expected)
 })
