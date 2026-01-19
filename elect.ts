dbc380eae010103daa3b240637b9a0492bbf70b8
ronyhe <ronyheruth@wix.com>
2026-01-19 12:23:13 +0200

    initial mandates

--- elect.ts
+++ elect.ts
@@ -3,12 +3,14 @@ const ISRAEL_RULES = {
     threshold: 0.0325 // 3.25%
 }
 
-interface Options {
+type Pair<T> = [T, T]
+
+interface Inputs {
     votes: {
         [party: string]: number
     }
     threshold: number // Achuz HaChasima
-    remainderPacts: string[][] // Heskemei Odafim
+    remainderPacts: Pair<string>[] // Heskemei Odafim
     totalSeats: number // 120 seats in Knesset
 }
 
@@ -18,8 +20,43 @@ interface Results {
     }
 }
 
-async function main() {
-    console.log('Election process started.')
+function runElection(options: Inputs): Results {
+    const { votes, threshold, remainderPacts, totalSeats } = options
+    const totalVotes = sum(Object.values(votes))
+    const effectiveThreshold = Math.floor(totalVotes * threshold) // Round? Up? Down?
+
+    const eligibleVotes = filterObject(votes, v => v >= effectiveThreshold)
+    const eligibleTotalVotes = sum(Object.values(eligibleVotes))
+
+    const pricePerSeat = Math.floor(eligibleTotalVotes / totalSeats) // Round? Up? Down? // HaModed
+
+    const initialSeats = mapObject(eligibleVotes, v => Math.floor(v / pricePerSeat)) // Round? Up? Down?
+    const totalInitialSeats = sum(Object.values(initialSeats))
+
+    const seatsLeft = totalSeats - totalInitialSeats
+
+    return { seats: {} }
 }
 
-await main()
+function sum(nums: number[]): number {
+    return nums.reduce((a, b) => a + b, 0)
+}
+
+function filterObject<K extends string | number | symbol, V>(
+    obj: Record<K, V>,
+    pred: (v: V, k: K) => boolean
+): Record<K, V> {
+    const pairs = Object.entries(obj) as [K, V][]
+    const filteredPairs = pairs.filter(([k, v]) => pred(v, k))
+    return Object.fromEntries(filteredPairs) as Record<K, V>
+}
+
+function mapObject<K extends string | number | symbol, A, B>(obj: Record<K, A>, fn: (v: A, k: K) => B): Record<K, B> {
+    const pairs = Object.entries(obj) as [K, A][]
+    const mappedPairs = pairs.map(([k, v]) => [k, fn(v, k)] as [K, B])
+    return Object.fromEntries(mappedPairs) as Record<K, B>
+}
+
+await (async function main() {
+    console.log('running')
+})()
