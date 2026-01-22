abd73a32ff7e9ddb0320d122d33d9a08a20368eb
ronyhe <ronyheruth@wix.com>
2026-01-22 13:14:15 +0200

    Cleanup

--- src/utils.ts
+++ src/utils.ts
@@ -18,27 +18,3 @@ export function maxBy<T>(arr: T[], fn: (item: T) => number): T | undefined {
 export function range(n: number): number[] {
     return [...new Array(n)].map((_, i) => i)
 }
-
-export function filterObject<K extends string | number | symbol, V>(
-    obj: Record<K, V>,
-    pred: (v: V, k: K) => boolean
-): Record<K, V> {
-    const pairs = Object.entries(obj) as [K, V][]
-    const filteredPairs = pairs.filter(([k, v]) => pred(v, k))
-    return Object.fromEntries(filteredPairs) as Record<K, V>
-}
-
-export function mapObject<K extends string | number | symbol, A, B>(
-    obj: Record<K, A>,
-    fn: (v: A, k: K) => B
-): Record<K, B> {
-    const pairs = Object.entries(obj) as [K, A][]
-    const mappedPairs = pairs.map(([k, v]) => [k, fn(v, k)] as [K, B])
-    return Object.fromEntries(mappedPairs) as Record<K, B>
-}
-
-export function sumValues(obj: Record<string, number>): number {
-    return sum(Object.values(obj))
-}
-
-export type Pair<T> = [T, T]
